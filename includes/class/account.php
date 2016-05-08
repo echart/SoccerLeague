9a5abc36b5145885f93bca9957f21743ebf29664
Echart <willians.fagundes@hotmail.com>
2016-05-08 12:00:52 -0300

    user authentication started

--- includes/class/account.php
+++ includes/class/account.php
@@ -61,7 +61,7 @@ class CreateAccount{
 
 	public function __construct($c, $e,$p,$f='NULL'){
 		$this->email=$e;
-		$this->password=password_hash($p, PASSWORD_BCRYPT, $this->options);
+		$this->password=password_hash($p, PASSWORD_BCRYPT, CreateAccount::options);
 		$this->father=$f;
 		$this->connection=$c;
 	}
