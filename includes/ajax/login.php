665bbb3f2bd7e01ba568e12c5173db45e4e09f78
Echart <willians.fagundes@hotmail.com>
2016-05-14 17:11:23 -0300

    mudanças na sintaxe das classes createAccount e createClub

--- includes/ajax/login.php
+++ includes/ajax/login.php
@@ -3,7 +3,7 @@ header('Content-type: application/JSON');
 
 include('../class/connection.php');
 include('../class/account.php');
-include('../class/userentication.php');
+include('../class/authentication.php');
 include('../class/club.php');
 
 $email=$_POST['login'] ?? '';
