486604305ba530d06e320166e7f3e21d6c31aafe
Echart <willians.fagundes@hotmail.com>
2016-05-16 22:10:13 -0300

    fixed minors bug

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,13 @@
 <header>
 	<input type="checkbox" class="menu-mobile" id='menu-mobile'/>
 	<label class='menu-mobile' for='menu-mobile'><i></i></label>
+	<label class='config' for='menu-mobile'><i></i></label>
 	<ul class='submenu'>
 		<? $submenu->display(); ?>
 	</ul>
 </header>
 <div class='menu-sidebar'>
-	<div class='logo'><i></i></div>
+	<!-- <div class='logo'><i></i></div> -->
 	<ul class='menu'>
 		<li><a class='home' href=''><i></i><span>Sede</span></a></li>
 		<li><a class='club border' href=""><i></i><span>Clube</span></a>
@@ -19,7 +20,7 @@
 		<li><a class='community' href=""><i></i><span>Comunidade</span></a></li>
 		<li><a class='pm border' href=""><i></i><span>Mensagens</span><span class='qtd'>0</span></a></li>
 		<li><a class='pro' href=""><i></i><span>PRO</span><span class='qtd'>365 dias</span></a></li>
-		<li><a class='account border' href=""><i></i><span>Conta</span></a></li>
-		<!-- <li><a class='logoff border' href="<?=$tree.'logout.php'?>"><i></i><span>Sair</span></a></li> -->
+<!-- 		<li><a class='account border' href=""><i></i><span>Conta</span></a></li>
+ -->		<li><a class='logoff border' href="<?=$tree.'logout.php'?>"><i></i><span>Sair</span></a></li>
 	</ul>
 </div>
