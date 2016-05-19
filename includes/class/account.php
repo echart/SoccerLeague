bf18ef9b8e5c8a7752253997c44a25901bbaee78
Echart <willians.fagundes@hotmail.com>
2016-05-18 23:29:47 -0300

    createAccount PDO update fixing errors 0.1

--- includes/class/account.php
+++ includes/class/account.php
@@ -85,6 +85,6 @@ class CreateAccount{
 		$query->bindParam(3,$this->father);
 
 		$query->execute();
-		$this->id_account=Connection::connect()->lastInsertedID();
+		$this->id_account=Connection::connect()->lastInsertID();
 	}
 }
