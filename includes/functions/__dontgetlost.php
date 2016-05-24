847ff6b518da1f48761724a3a36b8914cd4fe7c2
willians echart <willians.fagundes@hotmail.com>
2016-05-24 11:22:45 -0300

    __dontgetlost.php fixed and add __rothpath function

--- includes/functions/__dontgetlost.php
+++ includes/functions/__dontgetlost.php
@@ -7,9 +7,23 @@
 		$__tree=str_replace($_SERVER['DOCUMENT_ROOT'], '', $_SERVER['SCRIPT_FILENAME']);
 		//get the scriptname and exclud it from url;
 		$__tree=str_replace($script, '', $__tree);
-		//return just the path
+		//return just the current path
 		//ex: /var/www/soccerleague/includes/index.php will return just includes
 		return $__tree;
 	}
 
-	echo __dontgetlost('__dontgetlost.php');
+	/*
+		return the way  the root path
+	*/
+	function __rootpath($script):string{
+		//get the current path annd count the '/' repeat
+		$i=substr_count(__dontGetLost($script), '/')-1;
+		// make the the way to the root based on the result of i
+		$__url='';
+		while($i>0){
+			$__url.='../';
+			$i--;
+		}
+		//return the way to the root path
+		return $__url;
+	}
