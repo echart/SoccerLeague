5dcdfa66712eb916cd21329e44532ed7cbb22774
Echart <willians.fagundes@hotmail.com>
2016-06-14 17:24:38 -0300

    index support zepto or jquery

--- index.php
+++ index.php
@@ -114,8 +114,7 @@ if($user->verifyAuthentication()===true){
 	    }
 	];
 	</script>
-	<script>
-	document.write('<script src=' +('__proto__' in {} ? 'zepto' : 'jquery') +'.js><\/script>')</script>	
+	<script>document.write('<script src=assets/js/' +('__proto__' in {} ? 'zepto' : 'jquery') +'.js><\/script>')</script>	
 	<script type="text/javascript" src='assets/js/select.js'></script>
 	<script type="text/javascript" src='assets/js/home/home.min.js'></script>
 </body>
