f24c691eef7e1912ac39b575c9fa878da7ce9021
Echart <willians.fagundes@hotmail.com>
2016-06-30 15:31:50 -0300

    register controller update

--- controllers/_register.php
+++ controllers/_register.php
@@ -1,48 +1,59 @@
 <?
-include('../helpers/__autoload.php');
+require_once('../class/Validation.php');
+require_once('../class/Connection.php');
+require_once('../class/Account.php');
+require_once('../class/Club.php');
+require_once('../class/JsonOutput.php');
 
-$refeer=$_POST['refeer'] ?? '';
-$email=$_POST['login'] ?? '';
-$pass=$_POST['password'] ?? '';
-$pass2=$_POST['rpassword'] ?? '';
-$clubname=$_POST['clubname'] ?? '';
-$country=$_POST['country'] ?? '';
-
-JsonOutput::jsonHeader();
 
 try{
-	if($pass!=$pass2){
+	$refeer=$_POST['refeer'] ?? '';
+	$email=$_POST['login'] ?? '';
+	$pass=$_POST['password'] ?? '';
+	$pass2=$_POST['rpassword'] ?? '';
+	$clubname=$_POST['clubname'] ?? '';
+	$country=$_POST['country'] ?? '';
 
-	}else if($email=='' or $pass==''){
-		
-	}else if(strlen($pass)<8){
-		
-	}else if($clubname=='' or $country==''){
-	
+	$error='';
+	$error=Validation::isEqual($pass,$pass2);
+	$error.=Validation::isEmpty($email);
+	$error.=Validation::isEmpty($pass);
+	$error.=Validation::isEmpty($clubname);
+	$error.=Validation::minLenght($clubname,8);
+	$error.=Validation::isEmpty($country);
+	if($error!=''){
+		throw new Exception("Has an error with your params", 1);	
 	}
 }catch(Exception $e){
-	echo json_encode($return);
+	echo JsonOutput::error('exception',$e->getmessage());
 	exit;
 }
 
 try{
-	Connection::getInstance();
+	JsonOutput::jsonHeader();
+	$con=Connection::getInstance();
 	$account=new Account();
 	$account->setEmail($email);
-	if(($account->validEmail()===true) AND (Club::validClubName($clubname)===true)){
-		$account->setPassword($pass);
-		$account->setRefeer($refeer);
+	if(($account->validEmail()===true)){
+		if((Club::validClubName($clubname)===true)){
+			$account->setPassword($pass);
+			$account->setRefeer($refeer);
 
-		$club=new Club();
-		$club->id_account=$account->create();
-		$club->setClubeName($clubname);
-		$club->setCountry($country);
+			$club=new Club();
+			$club->id_account=$account->create();
+			$club->setClubeName($clubname);
+			$club->setCountry($country);
+			$club->create();
 
-		$club->create();
+			echo Json::success(array('clubname'=>$club->clubname));
+		}else{
+			throw new Exception("Invalid clubname", 1);
+		}
+	}else{
+		throw new Exception("Invalid email", 1);
 	}
 }catch(Exception $e){
-	$return=array('return'=>$e->getMessage());
+	echo Json::error('exception',$e->getmessage());
 }finally{
-	echo json_encode($return);
 	$con->disconnect();	
 }
