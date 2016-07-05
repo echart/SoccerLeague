94cb7c8485e37a4c08fdb06db3fd645c926b51fd
willians echart <willians.fagundes@hotmail.com>
2016-07-05 10:32:12 -0300

    changes in validation class

--- class/Validation.php
+++ class/Validation.php
@@ -1,20 +1,63 @@
 <?
 
 class Validation{
+	public static $errors=false;
+	public static $errorsMsg=array();
+	public static $errorsNum=0;
+
+	private static $instance;
+	public static function getInstance() {
+		if(!self::$instance) {
+			self::$instance = new self();
+			self::$errors=false;
+		}
+		return self::$instance;
 	
-	public static function isEqual($a, $b){
-		if($a==$b){
-			return true;
-		}else{
-			return false;
+	private function __construct() {
+
+	}
+	//avoid duplicate object
+	private function __clone() {}
+
+	public static function numeric($a){
+		if(!is_numeric($a)){
+			self::$erros=true;
+			self::$errorsMsg[]="This is not a numeric";
+			self::$errorsNum++;
 		}
 	}
-	public static function isEmpty($a){
-		if($a===''){
-			return true;
-		}else{
-			return false;
+
+	public static function string($a){
+		if(!is_string($a)){
+			self::$erros=true;
+			self::$errorsMsg[]="This is not a string";
+			self::$errorsNum++;
 		}
+	}
+
+	public static function isEqual($a, $b){
+		if($a!=$b and self::$errors!=false){
+			 self::$erros=true;
+			 self::$errorsMsg[]="They aren't equal";
+			 self::$errorsNum++;
+		}
+		return $self::$instance;
+	}
+	public static function isEmpty($a){
+		if($a!='' or $a!=null){
+			self::$erros=true;
+			 self::$errorsMsg[]="This is not empty!";
+			 self::$errorsNum++;
+		}
+		return self::$instance;
+	}
+	public static function isNotEmpty($a){
+		if($a=='' or $a==null){
+			self::$erros=true;
+			 self::$errorsMsg[]="This is empty!";
+			 self::$errorsNum++;
+		}
+		return self::$instance;
 	}
 	public static function lenght($a){
 		if(is_string($a)){
