3cdbd4d1439cb5ead699e1750d7d82b79aa96f2c
willians echart <willians.fagundes@hotmail.com>
2016-07-10 23:29:11 -0300

    update in handler for dont require controller, if controller doesnt exists, be cool

--- class/Handler.php
+++ class/Handler.php
@@ -4,19 +4,19 @@ class Handler{
 	public $request;
 	public $requestURL;
 	public $response;
-	
+
 	function requestUrl(array $request){
 		$this->requestURL=$request['request'];
 		$this->request=$request;
 	}
 	public function loadController(){
 		//load the controller
-		require_once('controllers/'.$this->requestURL.'.php');
+		include('controllers/'.$this->requestURL.'.php');
 	}
 	public function loadView(){
 		$view = new View($this->requestURL);
 		$data['tree']=__rootpath($_SERVER['REDIRECT_URL']);
-		
+
 		$view->setContentHead($data);
 		$view->loadHead();
 
