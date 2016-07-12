d390023472589b80a455863ce940b60d560cd1cb
willians echart <willians.fagundes@hotmail.com>
2016-07-12 09:05:32 -0300

    menu links fixed for now

--- class/Handler.php
+++ class/Handler.php
@@ -4,29 +4,23 @@ class Handler{
 	public $request;
 	public $requestURL;
 	public $response;
-
 	function requestUrl(array $request){
 		$this->requestURL=$request['request'];
 		$this->request=$request;
 	}
 	public function loadController(){
-		//load the controller
 		if(file_exists('controllers/'.$this->requestURL.'.php'))
 			include_once('controllers/'.$this->requestURL.'.php');
 	}
 	public function loadView(){
 		$view = new View($this->requestURL);
 		$data['tree']=__rootpath($_SERVER['REDIRECT_URL']);
-
 		$view->setContentHead($data);
 		$view->loadHead();
-
 		$view->setContentHeader($data);
 		$view->loadHeader();
-
 		$view->setContentView($data);
 		$view->loadView();
-
 		$view->loadFooter();
 	}
 }
--- controllers/league.php
+++ controllers/league.php
@@ -9,10 +9,6 @@ if(!isset($this->request['country'])) // if country isnt set at url, make the re
 ?>
 <div class='content'>
   <?
-  echo 'Divisão: ' . $division . '<br>';
-  echo 'Grupo: ' .$group . '<br>';
-  echo 'País: ' . $country . '<br>';
-
   $query=Connection::getInstance()->connect()->prepare("SELECT cc.clubname,lt.pts FROM competition c inner join league l using(id_competition) inner join league_table lt using(id_league) inner join club cc using(id_club) inner join country ccc on ccc.id_country= c.id_country where c.id_competition_type=1 and ccc.abbreviation=:country and l.division=:division and l.divgroup=:group and c.season=1");
   $query->bindParam(':country',$country);
   $query->bindParam(':division',$division);
