53d2cd4943deb6d24fcead70aefe931b0a36cda6
willians echart <willians.fagundes@hotmail.com>
2016-07-12 08:52:03 -0300

    redirect in league in case that url isnt in format corretly

--- controllers/league.php
+++ controllers/league.php
@@ -2,6 +2,10 @@
 $division = $this->request['div'] ?? $_SESSION['SL_div'];
 $group = $this->request['group'] ?? $_SESSION['SL_group'];
 $country = strtoupper($this->request['country']) ?? strtoupper($_SESSION['SL_country']);
+
+
+if(!isset($this->request['country'])) // if country isnt set at url, make the redirect to club league set in session
+  header('location: http://'.$_SERVER['SERVER_NAME'].'/league/'.strtolower($_SESSION['SL_country']).'/'.$division.'/'.$group);
 ?>
 <div class='content'>
   <?
