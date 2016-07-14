984bbe59b45384726f927ac0d0cbd3a0781f7c46
willians echart <willians.fagundes@hotmail.com>
2016-07-14 00:10:42 -0300

    SeasonStart update, still not working. Now, there's a problema with league added

--- controllers/SeasonStart.php
+++ controllers/SeasonStart.php
@@ -5,32 +5,29 @@
   /*----------
   SET COMPETITION DATA
   -----------*/
-  $competition_type='L';
-  $query=Connection::getInstance()->connect()->query("SELECT id_competition FROM competition where type=:type");
-  $query->bindParam(':type',$competition_type);
-  $query->execute();
-  $data=$query->fetch(PDO::FETCH_OBJ);
-  $id_competition=$data->
+  $id_competition_type=1;
   $teams=18;
   $total_games=($teams*2)-2;
   /*-------
   GET SEASON AND + 1(NEXT);
   ---------*/
-  $query=Connection::getInstance()->connect()->query("SELECT season FROM season");
-  $query->execute();
-  if($query->rowCount()==0){
-    $season=1;
-  }else{
-    $data=$query->fetch(PDO::FETCH_OBJ);
-    $season=$data->season+1;
-  }
+  // $query=Connection::getInstance()->connect()->query("SELECT season FROM season");
+  // $query->execute();
+  // if($query->rowCount()==0){
+  //   $season=1;
+  //   $query->execute();
+  // }else{
+  //   $data=$query->fetch(PDO::FETCH_OBJ);
+  //   $season=$data->season+1;
+  // }
+  $season=1;
   /*-----
   GET ALL COUNTRIES
   -------*/
   $query=Connection::getInstance()->connect()->query("SELECT id_country FROM country");
   $query->execute();
   $countries=array();
-  while($data=$query->fetch(PDO::FETCH_OBJ){
+  while($data=$query->fetch(PDO::FETCH_OBJ)){
     $countries[]=$data->id_country;
   }
   /*------
@@ -40,7 +37,7 @@
     /*----
     ADD competition
     ------*/
-    $query->Connection::getInstance()->connect()->prepare("INSERT INTO competition (id_competition_type, season, id_country, totalclubs) values (:id_competition_type,:season,:id_country,:totalclubs)");
+    $query=Connection::getInstance()->connect()->prepare("INSERT INTO competition (id_competition_type, season, id_country, totalclubs) values (:id_competition_type,:season,:id_country,:totalclubs)");
     $query->bindParam(':id_competition_type',$id_competition_type);
     $query->bindParam(':season',$season);
     $query->bindParam(':id_country',$id_country);
@@ -51,15 +48,15 @@
     /*------
     COUNT CLUBS AND COUNT HOW MANY LEAGUES/GROUPS WE NEED TO ADD.
     ------*/
-    $query=Connection::getInstance()->connect()->prepare("SELECT * FROM clubs where id_country=:id_country");
+    $query=Connection::getInstance()->connect()->prepare("SELECT * FROM club where id_country=:id_country");
     $query->bindParam(':id_country',$id_country);
     $query->execute();
     $clubs=$query->rowCount();
     /*------
     IF WE DONT HAVE 18 CLUBS FOR A GROUP, CREATE MORE AVAILABLE teams
     -------*/
-    if(League::leftClubs()>0){
-      for($i=0;$i<League::leftClubs();$i++){
+    if(League::leftClubs($clubs)>0){
+      for($i=0;$i<League::leftClubs($clubs);$i++){
         Club::createAvailableTeam($id_country);
       }
     }
@@ -67,23 +64,26 @@
     /*------
     ADD league based in groups numbers
     --------*/
-    for($i=0;$i<$groups;$i++){
+    $next=array(1,1);
+    for($i=1;$i<=$groups;$i++){
       /*----
       add league
       -----*/
-      $query=Connection::getInstance()->connect()->prepare("INSERT INTO league (id_competition, name, division, group, totalgames, round) values(:id_competition,:name,:division,:group,:totalgames,0)");
-      $query->bindParam(':id_competition',$id_competition);
-      $query->bindParam(':name',$name);
-      $query->bindParam(':division',$division);
-      $query->bindParam(':group',$group);
-      $query->bindParam(':totalgames',$total_games);
-      $query->execute();
-      $id_league=Connection::getInstance()->connect()->lastInsertID('league_id_league_seq');
+      if(!League::checkIfLeagueAlreadyExists($season,$country,$next[0],$next[1])){
+        $league=League::createLeague($id_competition,'Campeonato Brasileiro',$country,$division,$group);
+      }
+      $league=new League($season,$country,$next[0],$next[1]);
+      $next=$league->nextAvailableDivAndGroup();
       /*-----
       add league_table
       ------*/
       if($season==1){
-        
+        $query=Connection::getInstance()->connect()->prepare("SELECT id_club FROM club where id_country:id_country");
+        $query->bindParam(':id_country',$id_country);
+        $query->execute();
+        while($data=$query->fetch(PDO::FETCH_OBJ)){
+          $league->joinClub($data->id_club);
+        }
       }else{
         # TODO: get all season data in the past, verified the positions and make new league tables;
       }
