a20d5026629578e3c10942dfce82648fe1d5b6e7
Echart <willians.fagundes@hotmail.com>
2016-07-15 15:55:10 -0300

    i forgot the githubSSH remote name, fuck!

--- controllers/deployment.php
+++ controllers/deployment.php
@@ -5,7 +5,7 @@
 	$commands = array(
 		'echo $PWD',
     'whoami',
-    'git pull github development',
+    'git pull githubSSH development',
     'git status',
     'git submodule sync',
     'git submodule update',
