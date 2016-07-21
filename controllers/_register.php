45aaff6673fd74791baea2850fc23b4ca9f96c09
echart <willians.fagundes@hotmail.com>
2016-07-21 10:54:41 -0300

    [BUG FIX] get id_club on register for create players.

--- controllers/_register.php
+++ controllers/_register.php
@@ -49,6 +49,7 @@ try{
 				if($response==false){
 					// TODO: deletar conta, já que houve erro na criação do clube
 				}else{
+						$id_club=Connection::getInstance()->connect()->lastInsertID('club_id_club_seq');
 						include('PlayerCreator.php');
 						echo JsonOutput::success(array('clubname'=>$club->getClubName()));
 				}
