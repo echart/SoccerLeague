81a29f029d9d33bd826a0172d196a93e99be8396
Echart <willians.fagundes@hotmail.com>
2016-07-22 14:05:15 -0300

    [UPDATE] if error with club create, delete account!

--- controllers/_register.php
+++ controllers/_register.php
@@ -7,6 +7,13 @@ require_once('../class/JsonOutput.php');
 
 
 try{
+	/**
+	 * DATA VALIDATION
+	 * passwords must be equals and not blank
+	 * email must be not blank
+	 * country must be not blank
+	 * clubname must be not blank and with minlenght 8
+	 */
 	if($_POST['refeer']!='') $refeer=$_POST['refeer'];else $refeer=NULL;
 	$email=$_POST['login'] ?? '';
 	$pass=$_POST['password'] ?? '';
@@ -18,7 +25,9 @@ try{
 	Validation::validate($email)->isNotEmpty();
 	Validation::validate($clubname)->isNotEmpty()->minLenght(8);
 	Validation::validate($country)->isNotEmpty();
-
+	/**
+	 * If any rules break, get errors and throw exception
+	 */
 	if(Validation::$errorsNum>0){
 		for ($i=0; $i < Validation::$errorsNum ; $i++) {
 			throw new Exception(Validation::$errorsMsg[$i]);
@@ -32,25 +41,40 @@ try{
 	JsonOutput::jsonHeader();
 	$con=Connection::getInstance();
 	$account=new Account();
+	/* set email */
 	$account->setEmail($email);
+	/* verify if email already exists in database*/
 	if(($account->validEmail()===true)){
+		/* verify if clubname already exists in database*/
 		if((Club::validClubName($clubname)===true)){
+			/*set data for account*/
 			$account->setPassword($pass);
 			$account->setRefeer($refeer);
 			$club=new Club();
+			/* creat account getting account id */
 			$club->id_account=$account->create();
 			if($club->id_account!=""){
+				/*set club data */
 				$club->setClubName($clubname);
 				$club->setCountry($country);
+				/*verify if we have available clubs for this country*/
 				if($club->checkAvailableClub()==0){
+					/* Create new league with new available clubs */
 					// TODO: criar nova liga/grupo
 				}
+				/* create clubs*/
 				$response=$club->create();
 				if($response==false){
-					// TODO: deletar conta, já que houve erro na criação do clube
+					/**
+					 * delete account because we have an error with club creation
+					 */
+					$account->delete();
+					throw new Exception("We have an error :( #sad", 1);
 				}else{
+						/* create players for this club */
 						$id_club=$club->id_club;
 						include('PlayerCreator.php');
+						/* YAY. All process done! club created! return it.*/
 						echo JsonOutput::success(array('clubname'=>$club->getClubName()));
 				}
 			}
@@ -61,7 +85,9 @@ try{
 		throw new Exception("Invalid email", 1);
 	}
 }catch(Exception $e){
+	/* return if we have an exception :/ */
 	echo JsonOutput::error('exception',$e->getmessage());
 }finally{
+	/* disconnect database */
 	$con->disconnect();
 }
