63d53fc718f59cce3b04315a5a3fe399bf5665b5
willians echart <willians.fagundes@hotmail.com>
2016-07-23 18:31:35 -0300

    html style guide in home page

--- views/home.php
+++ views/home.php
@@ -1,3 +1,23 @@
-<div class='content'>
-  <h1>Home</h1>
-</div>
+<main>
+  <div class='content'>
+    <h1>h1</h1>
+    <h2>h2</h2>
+    <h3>h3</h3>
+    <h4>h4</h4>
+    <h5>h5</h5>
+    <h6>h6</h6>
+    <button class='btn-medium bg-success'>medium</button>
+    <button class='btn-large bg-warning btn-border'>Large</button>
+    <button class='btn bg-alert'>normal</button>
+    <button class='btn-small'>small</button>
+    <section>
+      <div class='box'>
+        <div class='box-title'>div box title</div>
+        <div class='box-content'>
+          <p>div box content</p>
+          
+        </div>
+      </div>
+    </section>
+  </div>
+</main>
