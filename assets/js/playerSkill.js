f84f1896a905977bd43f2831f017c0aef84b51fd
Echart <willians.fagundes@hotmail.com>
2016-08-25 18:05:55 -0300

    fixed url in playerskills js

--- assets/js/playerSkill.js
+++ assets/js/playerSkill.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $('table tbody tr td').each(function(){
     if($(this).html()==19){
-       $(this).html("<img src='http://t.socceleague.com.br/assets/img/silverstar.png' width='16px'>");
+       $(this).html("<img src='http://t.soccerleague.com.br/assets/img/silverstar.png' width='16px'>");
     }else if($(this).html()==20){
-       $(this).html("<img src='http://t.socceleague.com.br/assets/img/goldstar.png' width='16px'>");
+       $(this).html("<img src='http://t.soccerleague.com.br/assets/img/goldstar.png' width='16px'>");
     }
   })
 });
