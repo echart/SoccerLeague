209dd552cf3b41459a055bb88e327fec49fd68ad
willians echart <willians.fagundes@hotmail.com>
2016-08-30 23:04:02 -0300

    changes for class apresentation

--- controllers/MatchTemporarily.php
+++ controllers/MatchTemporarily.php
@@ -2,7 +2,7 @@
 <?
 require_once('../class/Connection.php');
 require_once('../class/LeagueTable.php');
-$day='2016-08-24';
+$day='2016-08-26';
 // $day=date('Y-m-d');
 $query = Connection::getInstance()->connect()->prepare("SELECT id_calendar,c.id_competition_type FROM league_calendar inner join league using(id_league) inner join competition c using(id_competition) inner join calendar using(id_calendar) where c.id_competition_type=:id_competition_type and matchday=:day");
 $query->bindParam(":day",$day);
