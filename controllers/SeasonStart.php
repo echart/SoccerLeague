e74a528e59aaa45ddf2316c7d8c51a89ec439fdd
willians echart <willians.fagundes@hotmail.com>
2016-08-30 22:29:26 -0300

    changes for class apresentation

--- controllers/SeasonStart.php
+++ controllers/SeasonStart.php
@@ -4,7 +4,7 @@
   include('../class/League.php');
   include('../class/Competition.php');
   include('../class/GenerateFixture.php');
-  exit;
+  
   /*----------
   SET COMPETITION DATA
   -----------*/
