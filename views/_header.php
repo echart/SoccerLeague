424b364ca20dd2601ebfff6da21c614c4db00319
Echart <willians.fagundes@hotmail.com>
2016-10-27 16:16:16 -0200

    menu changes

--- views/_header.php
+++ views/_header.php
@@ -1,7 +1,88 @@
-<header class='grid-1'>
-  <div class='SL-logo grid-20'><i></i></div>
-</header>
-
 <div class='sidebar grid-20'>
+  <div class='SL-logo'><i></i></div>
+  <div class='navbar'>
+    <div class='navbar-item'>
+      <ul class='menu'>
+        <li>
+          <i class='home'></i><span>Home</span>
+        </li>
+      </ul>
+      <ul class='submenu'>
 
+      </ul>
+    </div>
+    <div class='navbar-item'>
+      <ul class='menu'>
+        <li>
+          <i class='club'></i><span>Clube</span>
+        </li>
+      </ul>
+      <ul class='submenu'>
+
+      </ul>
+    </div>
+    <div class='navbar-item'>
+      <ul class='menu'>
+        <li>
+          <i class='squad'></i><span>Equipe</span>
+        </li>
+      </ul>
+      <ul class='submenu'>
+
+      </ul>
+    </div>
+    <div class='navbar-item'>
+      <ul class='menu'>
+        <li>
+          <i class='league'></i><span>Competições</span>
+        </li>
+      </ul>
+      <ul class='submenu'>
+
+      </ul>
+    </div>
+    <div class='navbar-item'>
+      <ul class='menu'>
+        <li>
+          <i class='club'></i><span>Mercado</span>
+        </li>
+      </ul>
+      <ul class='submenu'>
+
+      </ul>
+    </div>
+    <div class='navbar-item'>
+      <ul class='menu'>
+        <li>
+          <i class='club'></i><span>SLVIP</span>
+        </li>
+      </ul>
+      <ul class='submenu'>
+
+      </ul>
+    </div>
+    <div class='navbar-item'>
+      <ul class='menu'>
+        <li>
+          <i class='club'></i><span>Comunidade</span>
+        </li>
+      </ul>
+      <ul class='submenu'>
+
+      </ul>
+    </div>
+    <div class='navbar-item'>
+      <ul class='menu'>
+        <li>
+          <i class='club'></i><span>Sair</span>
+        </li>
+      </ul>
+      <ul class='submenu'>
+
+      </ul>
+    </div>
+  </div>
 </div>
+<header class='grid-80'>
+
+</header>
--- views/index.php
+++ views/index.php
@@ -1,8 +1,8 @@
-<div class="grid-80">
-  <!-- <div class="grid-75">
+<div class="content grid-80">
+  <div class="grid-75">
       <div class="box box--new">.grid-75</div>
   </div>
   <div class="grid-25">
       <div class="box">.grid-25</div>
-  </div> -->
+  </div>
 </div>
