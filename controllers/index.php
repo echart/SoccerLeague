1744aba90a2093f9879c86f5dab5e94eff3e27ae
willians echart <willians.fagundes@hotmail.com>
2016-11-24 23:50:03 -0200

    index

--- controllers/index.php
+++ controllers/index.php
@@ -2,5 +2,6 @@
 $this->tree = __rootpath($_SERVER['REDIRECT_URL']);
 $refeer= $_GET['refeer']?? NULL;
 $this->data['log'] = $_SESSION['logged'] ?? 'não logado';
-$this->loadView(true);
+$this->addCSSFile('index.css');
+$this->loadView(false);
 exit;
--- views/index.php
+++ views/index.php
@@ -1,8 +1,71 @@
-<div class="content grid-80">
-  <div class="grid-75">
-      <div class="box box--new">.grid-75</div>
-  </div>
-  <div class="grid-25">
-      <div class="box">.grid-25</div>
-  </div>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+
+	<!-- Title and Tags-->
+	<meta charset='UTF-8'>
+	<meta name="viewport" content="width=device-width"/>
+
+	<meta name="description" content="Soccer Management Game" />
+	<meta name="keywords" content="soccer, management, football, league, game, soccergame, soccerleague" />
+	<meta name="author" content="SoccerLeague" />
+
+	<title>Soccer League - Soccer Management Game</title>
+	<link rel="icon" type="image/png" sizes="96x96" href="<?=$this->tree?>assets/img/icon.png">
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="<?=$this->tree?>assets/css/grid.css">
+	<link rel="stylesheet" type="text/css" href="<?=$this->tree?>assets/css/sidebar.css">
+	<link rel="stylesheet" type="text/css" href="<?=$this->tree?>assets/semantic/dist/semantic.min.css">
+	<link rel="stylesheet" type="text/css" href="<?=$this->tree?>assets/css/styles.css">
+	<!-- <link rel="stylesheet" type="text/css" href="<?=$this->tree?>assets/css/header.css">-->
+	<link rel="stylesheet" type="text/css" href="<?=$this->tree?>assets/css/fonts.css">
+	<? /*load css files*/ $this->loadCSSFiles(); ?>
+</head>
+<body>
+	<main>
+    <!-- topbar -->
+    <div class='nav grid-1'>
+      <div class='logo'></div>
+      <ul>
+        <li><a href='#login'>Login</a></li>
+        <li><a href="#signup">Cadastrar</a></li>
+      </ul>
+    </div>
+    <!-- slider -->
+    <div class='itens'>
+      <div class='item'>
+        <?=$this->data['log']?>
+      </div>
+    </div>
+    <!-- slider controll -->
+    <div class='slider'></div>
+    <!-- modals -->
+    <div class="ui modal login-modal">
+      <i class="close icon"></i>
+      <div class="header">
+        Entrar
+      </div>
+    </div>
+    <div class="ui modal signup-modal">
+      <i class="close icon"></i>
+      <div class="header">
+        Cadastrar
+      </div>
+    </div>
+</main>
+<!--JS-->
+<script src="<?=$this->tree?>assets/js/jquery.js"></script>
+<script src="<?=$this->tree?>assets/semantic/dist/semantic.min.js"></script>
+<script src="<?=$this->tree?>assets/js/sidebar.js"></script>
+<? $this->loadJSFiles(); ?>
+<script type="text/javascript">
+  $('a[href="#login"]').on('click',function(){
+    $('.ui.modal.login-modal').modal('show');
+  });
+  $('a[href="#signup"]').on('click',function(){
+    $('.ui.modal.signup-modal').modal('show');
+  })
+</script>
+</body>
+</html>
+<? Connection::getInstance()->disconnect();?>
