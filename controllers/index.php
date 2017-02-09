30cd5f7a8d4cf40e18e8534bb422508ab39f86d8
Gustavo <gustavolameirao@gmail.com>
2017-02-09 09:51:59 -0200

    index change

--- controllers/index.php
+++ controllers/index.php
@@ -2,9 +2,6 @@
 $this->tree = __rootpath($_SERVER['REDIRECT_URL']);
 $refeer= $_GET['refeer']?? NULL;
 $this->data['log'] = $_SESSION['logged'] ?? 'não logado';
-<<<<<<< HEAD
-=======
 $this->addCSSFile('index.css');
->>>>>>> 704368058fcf1972fcf600be56480f56c4e06be6
-$this->loadView(false);
+$this->loadView(true);
 exit;
